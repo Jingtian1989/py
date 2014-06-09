78f365d64b341f85244a154cf9c4991402984ba0
jingtian1989 <zjt415991957@qq.com>
2014-06-10 00:10:50 +0800

    Update

--- generator/generator.py
+++ generator/generator.py
@@ -5,16 +5,34 @@
 ##  
 ##
 
-
-def gen_fn(n):
+##  demo1
+##  generator function
+def generator(n):
     for i in range(n):
         #return the value and then suspend
         yield i ** 2
 
 #test
-for i in gen_fn(5):
+for i in generator(5):
     print(i)
 
+#output     0
+#           1
+#           4
+#           9
+#           16
+
+
+
+
+##  demo2
+##  generator expression
+genexp = (i**2 for i in range(5))
+
+#test
+for i in genexp:
+    print(i)
+    
 #output     0
 #           1
 #           4
