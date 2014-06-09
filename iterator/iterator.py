fc64a74e102319a1280e45533ef1db257a196f6b
jingtian1989 <zjt415991957@qq.com>
2014-06-09 23:31:54 +0800

    Update

---
+++ iterator/iterator.py
@@ -0,0 +1,33 @@
+##
+##    Container & Iterators
+##
+##    Basic Functions:
+##        next(), returns the next element
+##        __iter__(), returns the iterator itself
+##
+##    Excetpions:
+##        raise StopIteration
+##
+##
+
+
+class IntegerIterator(object):
+    def __init__(self, step):
+        self.step = step
+
+    def next(self):
+        """ Returns the next elements."""
+    
+        if self.step == 0:
+            raise StopIteration
+        self.step -= 1
+        return self.step
+
+    def __iter__(self):
+        """Returns the iterator itself"""
+        return self
+
+
+    
+for el in IntegerIterator(5):
+    print el
